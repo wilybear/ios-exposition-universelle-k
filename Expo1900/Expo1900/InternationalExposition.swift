1b7ae2d007ddcfa864faf9738b7832203e0a78f8
Kim Hyun Shik <samerj9712@gmail.com>
2022-01-10 22:04:44 +0900

    만국박람회 [STEP 1] wily.kim, hayden.chae (#3)
    * feat: step1 테이블 뷰 컨텐츠 에셋 추가
    
    - 컨텐츠 이미지 에셋 추가
    - 컨텐츠 json 에셋 추가
    
    * feat: step1 Json 모델 구현
    
    - 박람회 정보를 나타내는 InternationalExposition 추가
    - 출품작 정보를 나타내는 Masterpiece 추가

---
+++ Expo1900/Expo1900/InternationalExposition.swift
@@ -0,0 +1,16 @@
+//
+//  InternationalExposition.swift
+//  Expo1900
+//
+//  Created by kakao on 2022/01/10.
+//
+
+import Foundation
+
+struct InternationalExposition: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
