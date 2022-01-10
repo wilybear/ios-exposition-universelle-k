c6b52883a17078f451304af6be5bbc62a1cbb411
inju2403 <56947879+inju2403@users.noreply.github.com>
2022-01-10 16:33:38 +0900

    feat: [STEP1] Json model type 구현

---
+++ Expo1900/Expo1900/Models/UniversalExposition.swift
@@ -0,0 +1,14 @@
+//
+//  UniversalExposition.swift
+//  Expo1900
+//
+//  Created by 이승주 on 2022/01/10.
+//
+
+struct UniversalExposition: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
