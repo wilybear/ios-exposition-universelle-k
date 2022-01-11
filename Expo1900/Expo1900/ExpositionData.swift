0f6ce868101b10dbe816965056d4009299c7b46e
cheshire.kwon <cheshire.kwon@kakaocorp.com>
2022-01-11 11:52:00 +0900

    feat: ExpositionData의 visitors renaming 추가

--- Expo1900/Expo1900/ExpositionData.swift
+++ Expo1900/Expo1900/ExpositionData.swift
@@ -9,18 +9,9 @@ struct ExpositionData: Codable {
     
     private enum CodingKeys: String, CodingKey {
         case title
-        case visitorsCount
+        case visitorsCount = "visitors"
         case location
         case duration
         case description
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.title = try container.decode(String.self, forKey: .title)
-        self.visitorsCount = try container.decode(Int.self, forKey: .visitorsCount)
-        self.location = try container.decode(String.self, forKey: .location)
-        self.duration = try container.decode(String.self, forKey: .duration)
-        self.description = try container.decode(String.self, forKey: .description)
     }
 }
